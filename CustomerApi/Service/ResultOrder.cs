4a370f0371f09b50a387b66afbdbbd41c4e4cc76
padmasgitrepo <padma96eee@gmail.com>
2024-05-23 13:17:53 +0530

    Add files via upload

---
+++ CustomerApi/Service/ResultOrder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace CustomerApi.Service
+{
+	public class ResultOrder
+	{
+		public int orderno;
+		public DateTime orderdate;
+		public Address address;
+		public OrderItem OrderItem;
+		public DateTime delivery_expected;
+		public List<KeyValuePair<string, object>> Order(int orderno, DateTime orderdate, Address address, OrderItem orderitem, DateTime Delivery_Expected)
+		{
+			List<KeyValuePair<string, object>> result_order = new List<KeyValuePair<string, object>>();
+			result_order.Add(new KeyValuePair<string, object>("OrderNumber", orderno));
+            result_order.Add(new KeyValuePair<string, object>("OrderDate", orderdate));
+            result_order.Add(new KeyValuePair<string, object>("Delivery Address", address));
+            result_order.Add(new KeyValuePair<string, object>("Order Items", orderitem));
+            result_order.Add(new KeyValuePair<string, object>("Expected Date", Delivery_Expected));
+			return result_order;
+        }
+        public ResultOrder()
+		{
+		}
+	}
+}
+
